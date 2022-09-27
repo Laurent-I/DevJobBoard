b34fabf532cb91bb8de12f6d3c5e9ad9b80f3d94
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-27 22:05:28 +0200

    Dynamic profile images

--- src/components/Profile/ProfileImage.js
+++ src/components/Profile/ProfileImage.js
@@ -1,16 +1,25 @@
 import React from "react";
 
 const ProfileImage = (props) => {
+  const imageFound = (
+    <img src={props.image} alt="Profile" className="rounded-full" />
+  );
+
+  const imageNotFound = (
+    <img
+      src="https://avatars.dicebear.com/api/bottts/pic.svg"
+      alt="Profile"
+      className="rounded-full"
+    />
+  );
+
   return (
     <div
       className="rounded-full border-4 border-blue-600 w-32 h-32 mx-16 my-5 flex flex-col hover:border-orange-400"
       id="image_container"
     >
-      <img
-        src={props.image || "https://avatars.dicebear.com/api/bottts/pic.svg"}
-        alt="Profile"
-        className="rounded-full"
-      />
+      {props.image && imageFound}
+      {!props.image && imageNotFound}
     </div>
   );
 };
